d6c1ca6b37c662840c17070b51592a0ea1c4359e
ParkSaseong <78298561+ParkSaseong@users.noreply.github.com>
2024-09-16 19:02:26 +0900

    feat : 커뮤니티 수정

--- app/src/main/java/com/example/daejangjung2/feature/main/community/CommunityFragment.kt
+++ app/src/main/java/com/example/daejangjung2/feature/main/community/CommunityFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.daejangjung2.R
 import com.example.daejangjung2.common.base.BindingFragment
 import com.example.daejangjung2.databinding.FragmentCommunityBinding
@@ -12,10 +13,17 @@ import com.example.daejangjung2.feature.main.mypage.MyPageFragment
 
 class CommunityFragment : BindingFragment<FragmentCommunityBinding>(R.layout.fragment_community){
     private val viewModel: CommunityViewModel by viewModels();
+    private lateinit var adapter: CommunityPostAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
+
+        // RecyclerView 설정
+        adapter = CommunityPostAdapter()
+        binding.communityRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.communityRecyclerView.adapter = adapter
+
         setupObserve()
     }
 
