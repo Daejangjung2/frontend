c81dcf6e8e7e9c418923ce79645a8849067e98cf
hangyeol <yulmam12345@gmail.com>
2024-09-16 18:50:27 +0900

    refactor: 페이지연동, 정규식

--- app/src/main/java/com/example/daejangjung2/feature/auth/login/LoginFirstActivity.kt
+++ app/src/main/java/com/example/daejangjung2/feature/auth/login/LoginFirstActivity.kt
@@ -1,25 +1,61 @@
 package com.example.daejangjung2.feature.auth.login
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
 import com.example.daejangjung2.R
 import com.example.daejangjung2.common.base.BindingActivity
 import com.example.daejangjung2.databinding.ActivityLoginBinding
+import com.example.daejangjung2.databinding.ActivityLoginFirstBinding
+import com.example.daejangjung2.feature.auth.signup.SignupSecondActivity
 import com.example.daejangjung2.feature.main.MainActivity
 
-class LoginFirstActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login_first ) {
+class LoginFirstActivity : BindingActivity<ActivityLoginFirstBinding>(R.layout.activity_login_first) {
     private val viewModel: LoginViewModel by viewModels { LoginViewModel.Factory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        if(viewModel.isLogin.value){
+        // 모든 입력 필드에 대해 텍스트 변경 리스너를 추가
+        binding.etFindEmail.addTextChangedListener { checkFieldsForEmptyValues() }
+
+        binding.btnContinue.setOnClickListener {
+            // 버튼이 활성화되어 있을 때만 다음 화면으로 이동
+            if (binding.btnContinue.isEnabled) {
+                val intent = Intent(this, LoginSecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    // 모든 필드의 빈칸 여부를 확인하고 버튼 상태를 변경하는 함수
+    private fun checkFieldsForEmptyValues() {
+        val email = binding.etFindEmail.text.toString().trim()
+
+        val allFieldsFilled = email.isNotEmpty()
+
+        // 모든 필드가 비어있지 않으면 버튼 활성화
+        if (allFieldsFilled) {
+            binding.btnContinue.isEnabled = true
+            binding.btnContinue.setBackgroundColor(Color.parseColor("#4682FF")) // 진한 파란색으로 변경
+        } else {
+            binding.btnContinue.isEnabled = false
+        }
+
+
+
+        if (viewModel.isLogin.value) {
             login()
         }
     }
 
-    private fun login(){
+    private fun login() {
         startActivity(MainActivity.getIntent(this@LoginFirstActivity))
         finish()
     }
