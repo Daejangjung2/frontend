6f08b7a601cb9505fe13cd2ed8a109898bacadd6
dmltn <dmltn3426@naver.com>
2024-09-21 00:52:21 +0900

    feature: 지도 경로 분할 및 지도 그리기

--- app/src/main/java/com/example/daejangjung2/feature/main/map/MapFragment.kt
+++ app/src/main/java/com/example/daejangjung2/feature/main/map/MapFragment.kt
@@ -1,17 +1,18 @@
 package com.example.daejangjung2.feature.main.map
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
-import com.google.android.gms.location.LocationRequest
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.View
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
@@ -19,12 +20,13 @@ import com.example.daejangjung2.R
 import com.example.daejangjung2.common.base.BindingFragment
 import com.example.daejangjung2.common.util.LocationUtils
 import com.example.daejangjung2.common.view.Toaster
-import com.example.daejangjung2.common.view.showSnackbar
 import com.example.daejangjung2.databinding.FragmentMapBinding
 import com.example.daejangjung2.domain.model.GeoPoint
+import com.example.daejangjung2.feature.main.map.guide.InformationMarker
 import com.example.daejangjung2.feature.main.map.newsInfo.NewsFragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.kakao.vectormap.KakaoMap
@@ -47,7 +49,8 @@ import com.kakao.vectormap.shape.PolygonStyles
 import com.kakao.vectormap.shape.PolygonStylesSet
 
 
-class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
+class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map),
+    SensorEventListener {
 
     private val viewModel: MapViewModel by viewModels()
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -57,10 +60,23 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     private var locationLabel: Label? = null
     private var headingLabel: Label? = null
     private var circleWavePolygon: Polygon? = null
+    private var lastLocation: Location? = null
+    private lateinit var sensorManager: SensorManager
+    private var rotationSensor: Sensor? = null
+
+    private val informationMarker: InformationMarker by lazy {
+        InformationMarker.getInstance()
+    }
+
+    private var sensorAccuracy: Int = SensorManager.SENSOR_STATUS_UNRELIABLE
 
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
+            locationResult ?: return  // null 체크
             for (location in locationResult.locations) {
+                Log.d("DAEJANG","제공자: ${location.provider}, ${location.latitude}, ${location.longitude}")
+                lastLocation = location
+                locationLabel?.moveTo(LatLng.from(location.latitude, location.longitude))
                 viewModel.updateCurPosition(GeoPoint(location.latitude, location.longitude))
                 break
             }
@@ -70,12 +86,17 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
         Maps = binding.kakaoMap;
+
+        setObserve();
 
         Maps.start(object : MapLifeCycleCallback() {
             override fun onMapDestroy() {
@@ -95,8 +116,6 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
             override fun onMapReady(kakao: KakaoMap) {
                 kakaoMap = kakao
 
-                kakaoMap.compass?.show()
-
                 trackingManager = kakaoMap.trackingManager!!
 
                 setupViewModel()
@@ -113,6 +132,10 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
         Maps.resume()
 
         checkLocationPermission()
+
+        rotationSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
 
         if (LocationUtils.isLocationPermissionGranted(requireContext())) {
             val locationRequest = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
@@ -140,6 +163,7 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     }
 
     private fun stopLocationUpdate(){
+        sensorManager.unregisterListener(this)
         fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
@@ -154,42 +178,45 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     }
 
     private fun setupViewModel() {
-        viewModel.lastUserPoint.observe(viewLifecycleOwner) { point ->
-            point?.let {
-                val lat = it.latitude;
-                val lon = it.longitude;
-                val position = LatLng.from(lat, lon)
-                val camera = CameraUpdateFactory.newCenterPosition(LatLng.from(lat, lon),17)
-                kakaoMap.moveCamera(camera, CameraAnimation.from(500, true, true));
+        val position = lastLocation?.let {
+            LatLng.from(it.latitude, it.longitude)
+        } ?: kakaoMap.cameraPosition?.position
 
-                val labelLayer = kakaoMap.labelManager?.layer
-                locationLabel = labelLayer?.addLabel(
-                    LabelOptions.from(position).setRank(10)
-                        .setStyles(LabelStyles.from(LabelStyle.from(R.drawable.current_location)
-                            .setAnchorPoint(0.5f, 0.5f)))
-                        .setTransform(TransformMethod.AbsoluteRotation_Decal)
-                )
+        val camera = CameraUpdateFactory.newCenterPosition(position,17)
+        kakaoMap.moveCamera(camera, CameraAnimation.from(500, true, true));
 
-                headingLabel = labelLayer?.addLabel(
-                    LabelOptions.from(position).setRank(9)
-                        .setStyles(LabelStyles.from(LabelStyle.from(R.drawable.red_direction_area)
-                            .setAnchorPoint(0.5f, 1.0f)))
-                        .setTransform(TransformMethod.AbsoluteRotation_Decal)
-                )
+        val labelLayer = kakaoMap.labelManager?.layer
+        locationLabel = labelLayer?.addLabel(
+            LabelOptions.from(position).setRank(10)
+                .setStyles(LabelStyles.from(LabelStyle.from(R.drawable.current_location)
+                    .setAnchorPoint(0.5f, 0.5f)))
+                .setTransform(TransformMethod.AbsoluteRotation_Decal)
+        )
 
-                locationLabel?.addSharePosition(headingLabel)
+        headingLabel = labelLayer?.addLabel(
+            LabelOptions.from(position).setRank(9)
+                .setStyles(LabelStyles.from(LabelStyle.from(R.drawable.red_direction_area)
+                    .setAnchorPoint(0.5f, 1.0f)))
+                .setTransform(TransformMethod.AbsoluteRotation_Decal)
+        )
 
-                circleWavePolygon = kakaoMap.shapeManager?.layer?.addPolygon(
-                    PolygonOptions.from("circlePolygon")
-                        .setVisible(false)
-                        .setDotPoints(DotPoints.fromCircle(position, 1.0f))
-                        .setStylesSet(PolygonStylesSet.from(PolygonStyles.from(Color.parseColor("#f55d44"))))
-                )
+        locationLabel?.addSharePosition(headingLabel)
 
-                locationLabel?.addShareTransform(circleWavePolygon)
-            }
-        }
+        circleWavePolygon = kakaoMap.shapeManager?.layer?.addPolygon(
+            PolygonOptions.from("circlePolygon")
+                .setVisible(false)
+                .setDotPoints(DotPoints.fromCircle(position, 1.0f))
+                .setStylesSet(PolygonStylesSet.from(PolygonStyles.from(Color.parseColor("#f55d44"))))
+        )
 
+        locationLabel?.addShareTransform(circleWavePolygon)
+    }
+
+    private fun showToast(@StringRes messageId: Int) {
+        Toaster.showShort(requireContext(), requireContext().getString(messageId))
+    }
+
+    private fun setObserve(){
         viewModel.event.observe(viewLifecycleOwner) { event ->
             handleEvent(event)
         }
@@ -200,7 +227,10 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
             is MapViewModel.Event.Failed -> TODO()
             is MapViewModel.Event.Service -> {
                 when(event.service){
-                    ServiceType.GUIDE -> setupButtonListeners(true, false, false, false)
+                    ServiceType.GUIDE -> {
+                        setupButtonListeners(true, false, false, false)
+                        informationMarker.setupRouterLine(kakaoMap, viewModel.lastUserPoint.value!!)
+                    }
                     ServiceType.WEATHER -> setupButtonListeners(false, true, false, false)
                     ServiceType.NEWS -> {
                         setupButtonListeners(false, false, true, false)
@@ -213,6 +243,37 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
         }
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        // 정확도가 높음(SENSOR_STATUS_ACCURACY_HIGH)일 때만 처리
+        if ( sensorAccuracy == SensorManager.SENSOR_STATUS_ACCURACY_HIGH) {
+            if (event?.sensor?.type == Sensor.TYPE_ORIENTATION) {
+                val rotationMatrix = FloatArray(9)
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+
+                val orientationAngles = FloatArray(3)
+                SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+                val azimuth = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+
+                headingLabel?.rotateTo(azimuth)
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        when (accuracy) {
+            SensorManager.SENSOR_STATUS_UNRELIABLE, SensorManager.SENSOR_STATUS_ACCURACY_LOW, SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM -> {
+                // 정확도가 낮거나 신뢰할 수 없으면 변화를 무시
+                sensorAccuracy = SensorManager.SENSOR_STATUS_UNRELIABLE
+            }
+            SensorManager.SENSOR_STATUS_ACCURACY_HIGH-> {
+                // 정확도가 보통이거나 높으면 허용
+                sensorAccuracy = accuracy
+            }
+        }
+    }
+
+
     private fun setupButtonListeners(isGuide: Boolean, isWeather: Boolean, isNews: Boolean, isContent: Boolean){
         binding.btnGuide.isSelected = isGuide
         binding.btnWeather.isSelected = isWeather
@@ -223,7 +284,6 @@ class MapFragment : BindingFragment<FragmentMapBinding>(R.layout.fragment_map) {
     companion object {
         private const val PERMISSIONS_REQUEST_CODE = 100
         private const val MAP_NEWS_INFO_TAG = "fragment_map_news_info_tag"
-        private const val DAEJANGJUNG: String = "DAJANGJUNG"
         private val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
 
         @JvmStatic
